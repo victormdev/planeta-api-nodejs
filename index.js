ac21b387b8a0f4632752fa93530de0ebdc2fc204
Victor Macedo <victoralexandrino99@gmail.com>
2024-08-04 07:00:18 -0300

    config cors

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 const express = require('express');
-
 const bodyParser = require('body-parser');
-
 const authRoutes = require('./routes/auth');
-
 const errorController = require('./controllers/error');
-
 const cors = require('cors');
 
 const app = express();
-
 const ports = process.env.PORT || 3000;
 
+// Configurar CORS
 app.use(cors());
+
+// Configurar body-parser
 app.use(bodyParser.json());
 
-app.use((req, res, next) =>{
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
+// Rotas
 app.use('/auth', authRoutes);
 
+// Tratamento de erros
 app.use(errorController.get404);
-
 app.use(errorController.get500);
 
+// Iniciar o servidor
 app.listen(ports, () => console.log(`Rodando na porta ${ports}`));
